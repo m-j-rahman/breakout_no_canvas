1e3b5216e81557cf52cf2475193fd48b45d6663a
UmairShah99 <umairshah14@hotmail.com>
2022-08-04 17:21:44 +0100

    test

--- js/tetris.js
+++ js/tetris.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
+
+    const shapes = new Array();
     const gridWidth = 10
     const gridHeight = 20
     const gridSize = gridWidth * gridHeight
-    const finalGrid = createGrid();
+    var colors = ['black', 'orange', 'red', 'blue'];
+    createBoard()
 
     function createGrid() {
         // the main grid
@@ -25,5 +28,35 @@ document.addEventListener('DOMContentLoaded', () => {
             miniGrid.appendChild(gridElement);
         }
         return grid;
-    }
+}
+
+    function createShapes(){
+
+    var Tblock = [[1, 0], [0,1], [1,1],[2,1]]; // 't' shape
+    var Iblock = [[0, 0], [0, 1], [0, 2], [0, 3]]; // line
+    var Squareblock = [[0, 0], [0, 1], [1, 0], [1, 1]]; // 4 x 4 square
+    var Lblock = [[2,0], [0, 1], [1, 1], [2,1]]; // 'L' shape
+    
+    shapes.push(Tblock);
+    shapes.push(Iblock);
+    shapes.push(Squareblock);
+    shapes.push(Lblock);
+}
+
+
+function createIndividualShape(){
+    var randomShape = Math.floor(Math.random() * shapes.length);
+    var randomColor = Math.floor(Math.random() * colors.length);
+    var center = Math.floor(gridWidth / 2);
+    var shape = shapes[randomShape];
+    var location = center 
+
+    currentShape = {
+        shape: shape,
+        color: colors[randomColor],
+        location: location,
+        indexes: getBlockNumbers(shape, location)
+    };
+}
+
 });
