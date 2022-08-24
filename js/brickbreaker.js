8c0e89cd48a66210e2c45340524d2743ed5a4873
UmairShah99 <umairshah14@hotmail.com>
2022-08-24 13:58:38 +0100

    ok

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -56,26 +56,6 @@ let blocks = [
     new Block(450, 210),
 ]
 
-
-let blocks2 = [
-    new Block(10, 270),
-    new Block(120, 270),
-    new Block(230, 270),
-    new Block(340, 270),
-    new Block(450, 270),
-    new Block(10, 240),
-    new Block(120, 240),
-    new Block(230, 240),
-    new Block(340, 240),
-    new Block(450, 240),
-    new Block(10, 210),
-    new Block(120, 210),
-    new Block(230, 210),
-    new Block(340, 210),
-    new Block(450, 210),
-]
-
-
 //draw my blocks
 function addBlocks() {
   for (let i = 0; i < blocks.length; i++) {
@@ -303,4 +283,5 @@ document.addEventListener('keydown', (event) => {
             }
         }
     }
+  }
 });
